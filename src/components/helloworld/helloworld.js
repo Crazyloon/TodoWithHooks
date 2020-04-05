bf8d92bc87e465f13601a5566ceeff541e68715e
Russell <russdow1@hotmail.com>
2020-04-05 03:36:46 -0700

    Add Initial Dashboard

---
+++ src/components/helloworld/helloworld.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+export default function HelloWorld () {
+  const [greeting, setGreeting ] = useState('World');
+  const [count, setCount] = useState(0);
+
+  // styles
+  const container = {
+    padding: '50px'
+  }
+
+  return (
+    <div style={container}>
+      <p>Hello {greeting} version <button onClick={() => setCount(count + 1)}>1.{count}</button></p> 
+      <input type='text' value={greeting} onChange={(e) => setGreeting(e.target.value)}></input>
+    </div>
+  ); 
+};
